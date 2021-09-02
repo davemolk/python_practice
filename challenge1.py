a002afdc217220d84a16f2fb938ddffb1d65410a
davemolk <davemolk@gmail.com>
2021-09-02 04:55:19 -0600

    one liner challnege1

--- challenge1.py
+++ challenge1.py
@@ -14,15 +14,16 @@ missyElliot(arr); // You gotta flip and reverse this
 missyElliot(lyricsArr); // I put my thang down flip it and reverse it
 '''
 def m_e(lst):
-    missy = []
-    lst.reverse()
-    for word in lst:
-        missy.append(word[::-1])
-    return ' '.join(missy)
+    # missy = []
+    # lst.reverse()
+    # for word in lst:
+    #     missy.append(word[::-1])
+    # return ' '.join(missy)
+    return ' '.join(list(word[::-1] for word in lst)[::-1])
 
 
 arr = [ 'siht', 'esrever', 'dna', 'pilf', 'attog', 'uoY' ]
 
 lyricsArr = ['ti', 'esrever', 'dna', 'ti', 'pilf', 'nwod', 'gnaht', 'ym', 'tup', 'I']
 
-m_e(arr)
+print(m_e(arr))
